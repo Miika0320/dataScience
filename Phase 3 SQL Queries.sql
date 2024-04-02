e97061975a47060bc90ee1804ab905638d5e3311
miika0320 <mikaeladobie1@gmail.com>
2024-04-02 16:13:07 -0400

    Update Phase 3 SQL Queries.sql

--- Phase 3 SQL Queries.sql
+++ Phase 3 SQL Queries.sql
@@ -24,5 +24,28 @@ Group by l.neighbourhood, day
 order by l.neighbourhood
 
 /*
-* 
+* --ROll Up using neighbourhood, city, country per year
 */
+
+Select l.neighbourhood, avg(co."severityLevel") as safetyScore, Extract(year from d."Date") as year
+from "Criminal_Offences_Final" co
+join "Location" l on l."locationId" = co."locationId"
+join "Date" d on d."Date" = co."incidentDate"
+Group by l.neighbourhood, year
+order by l.neighbourhood
+
+Select l.city, avg(co."severityLevel") as safetyScore, Extract(year from d."Date") as year
+from "Criminal_Offences_Final" co
+join "Location" l on l."locationId" = co."locationId"
+join "Date" d on d."Date" = co."incidentDate"
+Group by l.city, year
+order by l.city
+
+Select l.province, avg(co."severityLevel") as safetyScore, Extract(year from d."Date") as year
+from "Criminal_Offences_Final" co
+join "Location" l on l."locationId" = co."locationId"
+join "Date" d on d."Date" = co."incidentDate"
+Group by l.province, year
+order by l.province
+
+
