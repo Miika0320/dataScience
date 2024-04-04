7bc8d242b72af1f61aef4b605f8d98be120d7354
miika0320 <mikaeladobie1@gmail.com>
2024-04-04 14:34:05 -0400

    Update Phase 3 SQL Queries.sql

--- SQL/Phase 3 SQL Queries.sql
+++ SQL/Phase 3 SQL Queries.sql
@@ -1,4 +1,4 @@
---Selecting the average safety scores of areas over the course of Year then Month, than day
+-- Selecting the average safety scores of areas over the course of Year then Month, than day
 
 Select l.neighbourhood, avg(f."SafetyScore"), Extract(year from f."date") as year
 from "Fact" f
@@ -34,7 +34,7 @@ join "Location" l on l."locationId" = f."locationId"
 Group by l.neighbourhood, day
 order by l.neighbourhood
 
---ROll Up do the bottom one first^
+-- ROll Up do the bottom one first^
 
 
 -- dice: all Fact table data from slice location of Ottawa South
@@ -43,7 +43,7 @@ where "locationId" = 36
 
 -- dice: all Fact data from slice location of Ottawa South from year 2022
 Select * from "Fact"
-where "locationId" = 43 and Extract(year from "date") = 2022
+where "locationId" = 36 and Extract(year from "date") = 2022
 
 -- dice: all Fact data from slice location of Ottawa South that had severe crimes
 Select * from "Fact"
@@ -57,7 +57,7 @@ where Extract(month from f."date") = 3
 Group by l.neighbourhood, year, month
 order by l.neighbourhood
 
---Roll up and slice:all neighbourhoods and their safety scores that have an avg rent of over 500 for various years
+-- Roll up and slice:all neighbourhoods and their safety scores that have an avg rent of over 500 for various years
 Select l.neighbourhood,avg(f."SafetyScore"), Extract(year from f."date") as year, f."Avg_Rent"
 from "Fact" f
 join "Location" l on l."locationId" = f."locationId"
@@ -65,7 +65,7 @@ where f."Avg_Rent">500
 Group by l.neighbourhood, year, f."Avg_Rent"
 order by l.neighbourhood
 
---Drill down and dice: Bells Corners and its safetyscores during the month of march for various years
+-- Drill down and dice: Bells Corners and its safetyscores during the month of march for various years
 Select l.neighbourhood, avg(f."SafetyScore"), Extract(year from f."date") as year, Extract(month from f."date") as month
 from "Fact" f
 join "Location" l on l."locationId" = f."locationId"
@@ -73,7 +73,7 @@ where Extract(month from f."date") = 3 and f."locationId" = 4
 Group by l.neighbourhood, year, month
 order by l.neighbourhood
 
---Roll up and dice:Hawthorne Meadows and its safety score for 2018
+-- Roll up and dice:Hawthorne Meadows and its safety score for 2018
 Select l.neighbourhood,avg(f."SafetyScore"), Extract(year from f."date") as year, f."Avg_Rent"
 from "Fact" f
 join "Location" l on l."locationId" = f."locationId"
