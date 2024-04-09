c6fe31e7cd004bad2f786568317910e28695a0f6
miika0320 <mikaeladobie1@gmail.com>
2024-04-08 22:24:39 -0400

    Create outliers.py

---
+++ Utils/outliers.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.svm import OneClassSVM
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_csv("dataMartId.csv")
+
+numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
+
+# Standardize the features
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df[numeric_columns])
+
+# Fit the one-class SVM model
+svm_model = OneClassSVM(nu=0.01)  # Adjust nu parameter as needed
+svm_model.fit(scaled_data)
+
+# Predict outliers
+outlier_prediction = svm_model.predict(scaled_data)
+
+# Extract outliers
+outliers = df[outlier_prediction == -1]
+
+# Print outliers
+print("Outliers:")
+print(outliers)
+
+file = open('Outliers.txt', 'a')
+file.write(outliers)
+file.close()
